72bb374adefae4b47b7ad0c059c8fe52526ad4cb
Tobias Leichtfried <tleichtfried@outlook.com>
2023-12-07 14:09:42 +0000

    electric network

--- demos/test_ode.cc
+++ demos/test_ode.cc
@@ -23,6 +23,24 @@ class MassSpring : public NonlinearFunction
   }
 };
 
+class ElectricNetwork : public NonlinearFunction
+{
+  size_t DimX() const override { return 2; }
+  size_t DimF() const override { return 2; }
+  
+  void Evaluate (VectorView<double> x, VectorView<double> f) const override
+  {
+    f(0) = (cos(100*M_PI*x(1))- x(0));
+    f(1) = 1;
+  }
+  
+  void EvaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
+  {
+    df = 0.0;
+    df(0,0) = -1;
+    df(0,1) = -(100*M_PI)*sin(100*M_PI*x(1));
+  }
+};
 
 int main()
 {
@@ -31,8 +49,10 @@ int main()
   Vector<double> y {2};
   y(0)=1;
   y(1)=0;
-  auto rhs = std::make_shared<MassSpring>();
+  auto rhs = std::make_shared<ElectricNetwork>();
   
   SolveODE_CN(tend, steps, y, rhs,
               [](double t, VectorView<double> y) { std::cout << t << "  " << y(0) << " " << y(1) << std::endl; });
+
+
 }

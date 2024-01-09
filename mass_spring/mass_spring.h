4bc168e02e767abbb2794ebbce095f3a368b0358
Tobias Leichtfried <tleichtfried@outlook.com>
2024-01-09 22:07:28 +0000

    feat: add beams

--- mass_spring/mass_spring.h
+++ mass_spring/mass_spring.h
@@ -55,12 +55,20 @@ public:
   std::array<Connector,2> connections;
 };
 
+class Beam
+{
+public:
+  double length; 
+  std::array<Connector,2> connections;
+};
+
 template <int D>
 class MassSpringSystem
 {
   std::vector<Fix<D>> fixes;
   std::vector<Mass<D>> masses;
   std::vector<Spring> springs;
+  std::vector<Beam> beams;
   Vec<D> gravity=0.0;
 public:
   void SetGravity (Vec<D> _gravity) { gravity = _gravity; }
@@ -84,12 +92,17 @@ public:
     return springs.size()-1;
   }
 
-
+  size_t AddBeam (Beam b) // double length, double stiffness, Connector c1, Connector c2)
+  {
+    beams.push_back (b); // Spring{length, stiffness, { c1, c2 } });
+    return beams.size()-1;
+  }
 
   
   auto & Fixes() { return fixes; } 
   auto & Masses() { return masses; } 
   auto & Springs() { return springs; }
+  auto & Beams() { return beams; }
 
   void GetState (VectorView<> values, VectorView<> dvalues, VectorView<> ddvalues)
   {
@@ -135,6 +148,11 @@ std::ostream & operator<< (std::ostream & ost, MassSpringSystem<D> & mss)
   for (auto sp : mss.Springs())
     ost << "length = " << sp.length << "stiffness = " << sp.stiffness
         << ", C1 = " << sp.connections[0] << ", C2 = " << sp.connections[1] << std::endl;
+
+  ost << "beams: " << std::endl;
+  for (auto b : mss.Beams())
+    ost << "length = " << b.length  
+        << ", C1 = " << b.connections[0] << ", C2 = " << b.connections[1] << std::endl;
   return ost;
 }
 
@@ -147,15 +165,18 @@ public:
   MSS_Function (MassSpringSystem<D> & _mss)
     : mss(_mss) { }
 
-  virtual size_t DimX() const { return D*mss.Masses().size(); }
-  virtual size_t DimF() const { return D*mss.Masses().size(); }
+  virtual size_t DimX() const { return D*mss.Masses().size() + mss.Beams().size(); }
+  virtual size_t DimF() const { return D*mss.Masses().size()+ mss.Beams().size();; }
   
   virtual void Evaluate (VectorView<double> x, VectorView<double> f) const
   {
     f = 0.0;
+    int countLambda =  mss.Beams().size();
     
     auto xmat = AsMatrix(x,mss.Masses().size(), D);
+    auto xlambda = x.Range(D*mss.Masses().size(),countLambda);
     auto fmat = AsMatrix(f,mss.Masses().size(), D);
+    auto flambda = f.Range(D*mss.Masses().size(),countLambda);
     
     for (size_t i = 0; i < mss.Masses().size(); i++)
       fmat.Row(i) = mss.Masses()[i].mass*mss.Gravity();
@@ -180,6 +201,34 @@ public:
         if (c2.type == Connector::MASS)
           fmat.Row(c2.nr) -= force*dir12;
       }
+
+    
+    for(size_t i=0; i<countLambda; i++){
+        Beam beam = mss.Beams()[i];
+        auto [c1,c2] = beam.connections;
+        Vec<D> p1, p2;
+        if (c1.type == Connector::FIX && c2.type == Connector::FIX)
+          throw std::invalid_argument("Two fixes cant be connected with a beam!");
+
+        if (c1.type == Connector::FIX)
+          p1 = mss.Fixes()[c1.nr].pos;
+        else
+          p1 = xmat.Row(c1.nr);
+        if (c2.type == Connector::FIX)
+          p2 = mss.Fixes()[c2.nr].pos;
+        else
+          p2 = xmat.Row(c2.nr);
+
+        //Add g' to derivation in x 
+        auto force = xlambda(i)*2*(p1-p2);
+        if (c1.type == Connector::MASS)
+          fmat.Row(c1.nr) += force;
+        if (c2.type == Connector::MASS)
+          fmat.Row(c2.nr) -= force;
+
+        //calculate derivation in lambda
+        flambda(i)=((p1-p2)*(p1-p2))-beam.length*beam.length;
+    }
 
     for (size_t i = 0; i < mss.Masses().size(); i++)
       fmat.Row(i) /= mss.Masses()[i].mass;

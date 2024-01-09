8062ae835748ce0bec9ab9be967f01e3e4805905
Tobias Leichtfried <tleichtfried@outlook.com>
2024-01-09 22:48:50 +0000

    chore: update  bla

--- mass_spring/mass_spring.cc
+++ mass_spring/mass_spring.cc
@@ -6,7 +6,7 @@ int main()
   mss.SetGravity( {0,-9.81} );
   auto fA = mss.AddFix( { { 0.0, 0.0 } } );
   auto mA = mss.AddMass( { 1, { 1.0, 0.0 } } );
-  mss.AddSpring ( { 1, 10, { fA, mA } }  );
+  mss.AddBeam ( { 1, { fA, mA } }  );
 
   auto mB = mss.AddMass( { 1, { 2.0, 0.0 } } );
   mss.AddSpring ( { 1, 20, { mA, mB } } );
@@ -15,18 +15,18 @@ int main()
 
 
   double tend = 10;
-  double steps = 1000;
+  double steps = 100000;
   
-  Vector<> x(2*mss.Masses().size());
-  Vector<> dx(2*mss.Masses().size());  
-  Vector<> ddx(2*mss.Masses().size());  
+  Vector<> x(2*mss.Masses().size()+1);
+  Vector<> dx(2*mss.Masses().size()+1);  
+  Vector<> ddx(2*mss.Masses().size()+1);  
 
   auto mss_func = std::make_shared<MSS_Function<2>> (mss);
   auto mass = std::make_shared<IdentityFunction> (x.Size());      
 
   mss.GetState (x, dx, ddx);
   
-  SolveODE_Newmark(tend, steps, x, dx,  mss_func, mass,
+  SolveODE_Alpha(tend, steps,0.8, x, dx, ddx, mss_func, mass,
                    [](double t, VectorView<double> x) { std::cout << "t = " << t
                                                              << ", x = " << Vec<4>(x) << std::endl; });
 }

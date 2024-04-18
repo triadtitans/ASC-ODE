3be2ec6c88b3e395ab8ae1b5ad9f5232548f0a75
Homomorpheus <e12225948@student.tuwien.ac.at>
2024-04-18 11:38:45 +0200

    fix: conceptionally correct equations in rigid_body_fem

--- rigid_body/rigid_body_fem.cc
+++ rigid_body/rigid_body_fem.cc
@@ -1,4 +1,4 @@
-#include "rigid_body.h"
+#include "rigid_body_fem.h"
 
 
 int main()
@@ -10,22 +10,21 @@ int main()
 
   q(1)=1; q(2)=0; q(3)=0; 
   q(5)=0; q(6)=1; q(7)=0; 
-  q(9)=0; q(10)=0; q(11)=1; 
+  q(9)=0; q(10)=0; q(11)=1;
   
   q(12)=0; q(13)=0;q(14)=0; q(15)=0; q(16)=0; q(17)=0; 
-  Vector<double> dq { 18 };
-  dq(0)=0.00001;
-  dq(10)=0.001;
-  dq(7)=-0.001;
+  Vector<double> phat(6);
+  phat = 0;
+  phat(0) = 0.1;
  
-  Vector<double> ddq { 18 };
-  MatrixView<double> inertia_matrix(3,3,inertia_matrix_data);
-  RigidBody rb(q,dq,ddq,1,Vec<3>{0,0,0},inertia_matrix);
+  Matrix<double> inertia_matrix(3, 3);
+  MatrixView<double> inertia_v (inertia_matrix);
+  RigidBody rb(q,phat,1,Vec<3>{0,0,0},inertia_v);
   std::cout << "listmass_matrix:={";
-  rb.simulate(tend,steps, [](double t, VectorView<double> q) { 
-                    std::cout<<std::fixed << t << ": Translation =" << q(0) << " ," << q(4) << ", "<<", " << q(8) << "} " << std::endl
-                      <<"\t"<< " Rotation: " << q(1) << " ," << q(2) << ", "<<", " << q(3) << "} " << std::endl
-                      <<"\t"<< "           " << q(5) << " ," << q(6) << ", "<<", " << q(7) << "} " << std::endl
+  rb.simulate(tend,steps, [](int i, double t, VectorView<double> q) { 
+                    std::cout<<std::fixed << t << ": Translation =" << q(0) << " ," << q(1) << ", "<<", " << q(2) << "} " << std::endl
+                      <<"\t"<< " Rotation: " << q(3) << " ," << q(4) << ", "<<", " << q(5) << "} " << std::endl
+                      <<"\t"<< "           " << q(6) << " ," << q(7) << ", "<<", " << q(8) << "} " << std::endl
                       <<"\t"<< "           " << q(9) << " ," << q(10) << ", "<<", " << q(11) << "} " << std::endl; }                   
                    );
   std::cout << "}";
--- rigid_body/rigid_body_fem.h
+++ rigid_body/rigid_body_fem.h
@@ -7,10 +7,31 @@
 
 constexpr size_t dim_per_body = 18;
 
-class RhsRigidBody;
+class RigidBody;
 
 using namespace ASC_bla;
 using namespace ASC_ode;
+
+template<typename T>
+auto hat_map(const VecExpr<T>& v){
+  Matrix<decltype(1.0*v(0))> M(3, 3);
+  M(0, 1) = -v(2);
+  M(0, 2) = v(1);
+  M(1, 0) = v(2);
+  M(1, 2) = -v(0);
+  M(2, 0) = -v(1);
+  M(2, 1) = v(0);
+  return M;
+}
+
+template<typename T>
+auto inv_hat_map(const MatrixExpr<T>& M){
+  Vector<decltype(1.0 * M(0, 0))> v(3);
+  v(0) = (M(2, 1) - M(1, 2))/2;
+  v(1) = (M(0, 2) - M(2, 0))/2;
+  v(2) = (M(1, 0) - M(0, 1))/2;
+  return v;
+}
 
 template<typename T = double>
 class Transformation{
@@ -31,12 +52,23 @@ class Transformation{
     if(i>2||j>2 || j<0 || i <0) throw std::invalid_argument("Rotation Matrix is 3x3");
     q_((4*i + 1) + j)=r; // note the ordering of q
   }
-
-  Vector<T> getTranslation(){
-    return Vector<T>{q_(0),q_(4),q_(8)};
+  template<typename S>
+  void setRotation(const MatrixExpr<S>& B){
+    for (size_t i=0; i < 3; i++){
+      for (size_t j=0; j < 3; j++){
+        setRotation(i, j, B(i, j));
+      }
+    }
   }
 
-  Matrix<T> getRotation() {
+  Vector<T> getTranslation() const{
+    Vector<T> v(3);
+    v(0)=q_(0);
+    v(1)=q_(4);
+    v(2)=q_(8);
+    return v;
+  }
+  Matrix<T> getRotation() const {
     Matrix<T> B(3, 3);
     for (size_t i=0; i < 3; i++){
       for (size_t j=0; j < 3; j++){
@@ -56,15 +88,79 @@ class Transformation{
   }
 
 
+class EQRigidBody : public NonlinearFunction
+{
+  double h_;
+  Transformation<double> Q_;
+  Vector<double> phatold;
+  public:
+  EQRigidBody(Transformation<double> Q, Vector<double> Phat, double h): h_(h), Q_(Q), phatold(Phat) {};
+  size_t DimX() const  { return 30; }
+  size_t DimF() const  { return 30; }
+
+  void Evaluate (VectorView<double> x, VectorView<double> f) const override
+  {
+    // x (a(3), B(row_maj)(9), v_trans(3), v_skew(3), phat(6), p(6))
+
+    // variables
+    Vector<double> anew = x.Range(0, 3);
+    MatrixView<double> Bnew = AsMatrix(x.Range(3, 12), 3, 3);
+    Vector<double> vtrans = x.Range(12, 15);
+    Vector<double> vskew = x.Range(15, 18);
+    Vector<double> phat = x.Range(18, 24);
+    Vector<double> p = x.Range(24, 30);
+
+    // known constants
+    Vector<double> aold = Q_.getTranslation();
+    Matrix<double> Bold = Q_.getRotation();
+
+    Matrix<double> Bhalf(3, 3);
+    Bhalf = 0.5*(Bnew + Bold);
+
+    // I
+    f.Range(0, 3) = (1/h_)*(anew - aold) - vtrans;
+    f.Range(3, 6) = (1/h_)*inv_hat_map(Transpose(Bhalf)*(Bnew - Bold)) - vtrans;
+
+    // II
+    Vector<double> vhat(6);
+    vhat.Range(0, 3) = vtrans;
+    vhat.Range(3, 6) = vskew;
+
+    // for the moment, M shall be id
+    f.Range(6, 12) = vhat - p;
+
+    // III
+    
+    f.Range(12, 15) = (2/h_)*(phat.Range(0, 3) - p.Range(0, 3));
+    f.Range(15, 18) = inv_hat_map((2/h_)*(Transpose(Bnew) * (Bnew*hat_map(phat.Range(3, 6)) - Bhalf*hat_map(p.Range(3, 6)))));
+    f.Range(18, 21) = (2/h_)*(p.Range(0, 3) - phatold.Range(0, 3));
+    f.Range(21, 24) = inv_hat_map((2/h_)*(Transpose(Bold) * (Bhalf*hat_map(p.Range(3, 6)) - Bold*hat_map(phatold.Range(3, 6)))));
+
+    // Bnew must be orthonormal
+    Matrix<double> eye = Diagonal(3, 1);
+    auto c = Transpose(Bnew)* Bnew + (-1)*eye;
+    f(0 + 24)=c(0, 0);
+    f(1 + 24)=c(1, 0);
+    f(2 + 24)=c(2, 0);
+    f(3 + 24)=c(1, 1);
+    f(4 + 24)=c(2, 1);
+    f(5 + 24)=c(2, 2);
+  }
+
+  void EvaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
+  {
+    dNumeric(*this, x, df);
+  }
+};
+
+
 class RigidBody {
   Vector<double> q_;
-  Vector<double> dq_;
   Vector<double> phat_;
   Vector<double> initialq_;
-  Vector<double> initialdq_;
   Vector<double> initialphat_;
 
-  Matrix<double> T;
+  Matrix<double> P_;
 
   Vec<3> center_of_mass_={0,0,0};
   Matrix<double> inertia_;
@@ -72,14 +168,14 @@ class RigidBody {
   std::shared_ptr<NonlinearFunction> mass_function;
 public:
 
-  RigidBody(Vector<double> q,Vector<double> dq,Vector<double> phat,double mass, Vec<3> center_of_mass,MatrixExpr<T>& inertia)
-        : q_(q),dq_(dq), phat_(phat), initialq_(q), initialdq_(dq), initialphat_(phat), center_of_mass_(center_of_mass),
-          mass_(mass), inertia_(inertia), T(6, 12) {
+  template<typename T>
+  RigidBody(Vector<double> q,Vector<double> phat,double mass, Vec<3> center_of_mass,MatrixExpr<T>& inertia)
+        : q_(q), phat_(phat), initialq_(q), initialphat_(phat), center_of_mass_(center_of_mass),
+          mass_(mass), inertia_(inertia), P_(6, 12) {
     if(inertia.Width() != 3) throw std::invalid_argument("Inertia matrix must be 3x3");
     if(inertia.Height() != 3) throw std::invalid_argument("Inertia matrix must be 3x3");
     if(q.Size() != dim_per_body) throw std::invalid_argument("q Vector must match mass matrix");
-    if(dq.Size() != dim_per_body) throw std::invalid_argument("q Vector must match mass matrix");
-    if(phat.Size() !=6) throw std::invalid_argument("q Vector must match mass matrix");
+    if(phat.Size() != 6) throw std::invalid_argument("q Vector must match mass matrix");
 
   }
 
@@ -112,22 +208,19 @@ public:
   */
 
   void setQ(Transformation<> t){q_=t.q_;}
-  void setDq(Transformation<> t){dq_=t.q_;}
-  void setPhat(Vector<double> v){phat_=T*v;}
+  void setPhat(Vector<double> v){phat_=v;}
 
   void setMass(Matrix<double> m){mass_function=std::make_shared<LinearFunction>(m);}
   std::shared_ptr<NonlinearFunction> getMassFunc(){return mass_function;}
   // saves a state for the reset button
   void saveState(){
     initialq_ = q_;
-    initialdq_ = dq_;
-    initialddq_ = ddq_;
+    initialphat_ = phat_;
   }
   // resets position and rotation to last saved state
   void reset(){
     q_ = initialq_;
-    dq_ = initialdq_;
-    ddq_ = initialddq_;
+    phat_ = initialphat_;
   }
 
   Vec<3> absolutePosOf(Vec<3> relative_pos){
@@ -135,93 +228,23 @@ public:
   }
 
   Transformation<> getQ(){return q_;}
-  Transformation<> getDq(){return dq_;}
-  Transformation<> getDdq(){return ddq_;}
+  Vector<double> getPhat(){return phat_;}
 
-  void simulate(double tend, double steps, std::function<void(double,VectorView<double>)> callback = nullptr ){
-    /* std::shared_ptr<RhsRigidBody> rhs = std::make_shared<RhsRigidBody>(*this);
-    //std::shared_ptr<NumericDerivative> dlagrange = std::make_shared<NumericDerivative>(rhs);
-    std::shared_ptr<Derivative> dlagrange = std::make_shared<Derivative>(rhs);
-   
-    SolveODE_Alpha (tend, steps, 0.8, q_, dq_, ddq_, dlagrange, mass_function, callback); */
-
-
+  void simulate(double tend, double steps, std::function<void(int,double,VectorView<double>)> callback = nullptr ){
+    Vector<double> state(30);
+    state.Range(0,3)=getQ().getTranslation();
+    AsMatrix(state.Range(3, 12), 3, 3) = getQ().getRotation();
+    state.Range(18, 24) = phat_;
+    for (size_t step=0; step < steps; step++){
+      auto q = getQ();
+      auto p = getPhat();
+      std::shared_ptr<EQRigidBody> eq = std::make_shared<EQRigidBody>(q, p, tend/steps);
+      NewtonSolver(eq, state, 1e-10, 10, callback);
+      Transformation<double> Q = getQ();
+      Q.setTranslation(state(0),state(1),state(2));
+      Q.setRotation(AsMatrix(state.Range(3, 12), 3, 3));
+      setQ(Q);
+      phat_ = state.Range(18, 24);
+    }
   } 
 };
-
-class EQRigidBody : public NonlinearFunction
-{
-  double h_;
-  RigidBody b_;
-  public:
-  //Just a reminder
-  EQRigidBody(RigidBody b, double h) h_(h), b_(b) {};
-  size_t DimX() const  { return 39; } //???
-  size_t DimF() const  { return 48; } //???
-
-  void Evaluate (VectorView<double> x, VectorView<double> f) const override
-  {
-    // x (a(3), B(row_maj)(9), v_trans(3), v_skew(3), phat(6), p(6), Bhalf(9))
-    Vector<double> anew = x.Range(0, 3);
-    MatrixView<double> Bnew = asMatrix(x.Range(3, 12), 3, 3);
-    Vector<double> vtrans = x.Range(12, 15);
-    Vector<double> vskew = x.Range(15, 18);
-    Vector<double> phat = x.Range(18, 24);
-    Vector<double> p = x.Range(24, 30);
-    MatrixView<double> Bhalf = asMatrix(x.Range(30, 39), 3, 3);
-
-    Vector<double> aold = b_.getQ().getTranslation();
-    Matrix<double> Bold = b_.getQ().getRotation();
-
-    f.Range(0, 3) = (1/h_)*(anew - aold) - vtrans;
-    MatrixView<double> f_temp(asMatrix(f.Range(3, 12)));
-    ftemp = (1/h_)*(Bnew - Bold) - Bhalf*vskew; // Bold oder B_{1/2}???
-    // M*(vtrans, vskew) = phatold
-    MatrixView<double> ftemp(asMatrix(f.Range(12, 21)));
-    ftemp = Bold*phatold - Bhalf*pold; // pold oder phatold???
-    MatrixView<double> ftemp(asMatrix(f.Range(21, 30)));
-    ftemp = Bnew*phat - Bhalf*pold;
-    MatrixView<double> ftemp(asMatrix(f.Range(30, 39)));
-    ftemp = Transpose(Bnew)*Bnew - Diagonal(3, 1);
-    MatrixView<double> ftemp(asMatrix(f.Range(39, 48)));
-    ftemp = Transpose(Bhalf)*Bhalf - Diagonal(3, 1);
-  }
-
-  void EvaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
-  {
-    
-  }
-};
-
-  
-
-
-
-/* 
-inline double mass_matrix_data[18*18] ={
-1.0, -0.0, -0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
--0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
--0.0, 0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 1.0, -0.0, -0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, -0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, -0.0, 0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, -0.0, -0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.0, 0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.08333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, \
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-};
-
-inline double inertia_matrix_data[3*3] ={
-0.08333, 0.0, 0.0, \
- 0.0, 0.08333, 0.0,\
- 0.0, 0.0, 0.08333, \
-}; */
-
